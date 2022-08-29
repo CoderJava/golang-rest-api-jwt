30d3bc011d40620a98f8667599f5e73b26e9a8a2
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-29 21:59:05 +0700

    Daftarkan API register user kedalam route

--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"golang-rest-api-jwt/config"
+	v1 "golang-rest-api-jwt/handler/v1"
+	"golang-rest-api-jwt/repository"
+	"golang-rest-api-jwt/service"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db := config.SetupDatabaseConnection()
 	defer config.CloseDatabaseConnection(db)
+
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	authHandler := v1.NewAuthHandler(userService)
+
+	server := gin.Default()
+	authRoutes := server.Group("api/auth")
+	{
+		authRoutes.POST("/register", authHandler.Register)
+	}
+
+	server.Run()
 }
