477b6f8602808dbedb56836da50e86725469389c
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-09-01 21:36:02 +0700

    Daftarkan API profile user kedalam router

--- main.go
+++ main.go
@@ -14,16 +14,24 @@ func main() {
 	defer config.CloseDatabaseConnection(db)
 
 	userRepository := repository.NewUserRepository(db)
+
 	userService := service.NewUserService(userRepository)
 	authService := service.NewAuthService(userRepository)
 	jwtService := service.NewJWTService()
+
 	authHandler := v1.NewAuthHandler(userService, authService, jwtService)
+	userHandler := v1.NewUserHandler(userService, jwtService)
 
 	server := gin.Default()
 	authRoutes := server.Group("api/auth")
 	{
 		authRoutes.POST("/register", authHandler.Register)
 		authRoutes.POST("/login", authHandler.Login)
+	}
+
+	userRoutes := server.Group("api/user")
+	{
+		userRoutes.GET("/profile", userHandler.Profile)
 	}
 
 	server.Run()
