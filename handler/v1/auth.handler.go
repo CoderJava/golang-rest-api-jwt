9c0743717b7f6ba40a949a0fc577bd3a2098f9b9
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-09-01 20:10:55 +0700

    Buat API login user

--- handler/v1/auth.handler.go
+++ handler/v1/auth.handler.go
@@ -6,21 +6,32 @@ import (
 	"golang-rest-api-jwt/dto"
 	"golang-rest-api-jwt/service"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type AuthHandler interface {
 	Register(ctx *gin.Context)
+
+	Login(ctx *gin.Context)
 }
 
 type authHandler struct {
 	userService service.UserService
+	authService service.AuthService
+	jwtService  service.JWTService
 }
 
-func NewAuthHandler(userService service.UserService) AuthHandler {
+func NewAuthHandler(
+	userService service.UserService,
+	authService service.AuthService,
+	jwtService service.JWTService,
+) AuthHandler {
 	return &authHandler{
 		userService: userService,
+		authService: authService,
+		jwtService:  jwtService,
 	}
 }
 
@@ -42,4 +53,26 @@ func (h *authHandler) Register(ctx *gin.Context) {
 
 	response := response.BuildSuccessResponse(true, "Success", user)
 	ctx.JSON(http.StatusCreated, response)
+}
+
+func (h *authHandler) Login(ctx *gin.Context) {
+	var loginRequest dto.LoginRequest
+	if err := ctx.ShouldBind(&loginRequest); err != nil {
+		response := response.BuildErrorResponse("Failed to process request", err.Error(), obj.EmptyObj{})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	if err := h.authService.VerifyCredential(loginRequest.Email, loginRequest.Pasword); err != nil {
+		response := response.BuildErrorResponse("Failed to login", err.Error(), obj.EmptyObj{})
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		return
+	}
+
+	user, _ := h.userService.FindUserByEmail(loginRequest.Email)
+
+	token := h.jwtService.GenerateToken(strconv.Itoa(int(user.ID)))
+	user.AccessToken = token
+	response := response.BuildSuccessResponse(true, "Success", user)
+	ctx.JSON(http.StatusOK, response)
 }
